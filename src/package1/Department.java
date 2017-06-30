570739f9ce444f1830a37a174f08439c75be9c1e
bhawani0642 <bhawani0642@gmail.com>
2017-06-30 15:36:52 +0530

    Assignment7.1

---
+++ src/package1/Department.java
@@ -0,0 +1,13 @@
+package package1;
+import packagea.Employee;
+
+public class Department extends Employee {
+	
+
+		public static void main(String[] args) {
+			
+			Department dp = new Department();
+			//Here i want to call the protected method
+			dp.Display();
+		}
+}
---
+++ src/packagea/Employee.java
@@ -0,0 +1,12 @@
+// package with name packagea
+package packagea;
+// Created a class named Employee
+public class Employee {
+int empId=25;
+String empName="Bhawani";
+// Creating the private method Display
+protected void Display(){
+	System.out.println(empId);
+	System.out.println(empName);
+}
+}
